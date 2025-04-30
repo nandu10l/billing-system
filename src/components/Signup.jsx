05c762cb2ce53dc5f55993fbd1f2e94261ac1de5
Nandagopan L <118618712+nandu10l@users.noreply.github.com>
2025-04-30 21:01:05 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,12 +1,22 @@
 // src/components/SignUp.jsx
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import { Link, useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function SignUp() {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // prevent page reload
+    toast.success('Account created successfully!', {
+      onClose: () => navigate('/'), // after toast closes, navigate to login
+      autoClose: 2000, // close after 2 seconds
+    });
+  };
   return (
     <div className="container">
       <h2 style={{ textAlign: "center" }}>Sign Up</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Full Name" required />
         <input type="email" placeholder="Email" required />
         <input type="password" placeholder="Password" required />
@@ -14,8 +24,11 @@ function SignUp() {
         <button type="submit">Sign Up</button>
       </form>
       <p style={{ textAlign: "center", marginTop: "10px" }}>
-        Already have an account? <Link to="/login">Login</Link>
+        Already have an account? <Link to="/">Login</Link>
       </p>
+      {/* Toast Container for showing toasts */}
+      <ToastContainer />
+
     </div>
   );
 }
